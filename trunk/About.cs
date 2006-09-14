ad14d2486012e66b6b823c400dd3128673acf4a6
guessman <guessman@4c4ae9e3-5f1d-0410-95e6-c0dd27e02d6a>
2006-09-14 14:15:19 +0000

    Corrected namespace

--- trunk/About.cs
+++ trunk/About.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.ComponentModel;
 using System.Windows.Forms;
 
-namespace QueryExpress
+namespace QueryExPlus
 {
 	public class About : System.Windows.Forms.Form
 	{
