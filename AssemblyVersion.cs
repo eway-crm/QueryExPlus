7995df24efc7e832d603920bf5a205f2b529565a
guessman <guessman@4c4ae9e3-5f1d-0410-95e6-c0dd27e02d6a>
2006-10-22 04:08:53 +0000

    increased version to 2.0.1.1

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.1.1")]
+[assembly: AssemblyFileVersion("2.0.1.1")]
 
 
